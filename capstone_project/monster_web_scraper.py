2aa3f460111a9dbc0653778f51c66b5d79c5744c
empdierks <emi.dierks@gmail.com>
2020-05-31 10:45:03 -0400

    cleaned up get_nums

--- capstone_project/monster_web_scraper.py
+++ capstone_project/monster_web_scraper.py
@@ -9,9 +9,6 @@ def get_nums(url):
 			'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
 	})
 
-	page = requests.get(url, headers=headers)
-	soup = BeautifulSoup(page.content, 'html.parser')
-
 	try:
 		page = requests.get(url, headers=headers)
 		soup = BeautifulSoup(page.content, 'html.parser')
